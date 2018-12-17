cbef9317d034c253c97b5d50055ff442c531edb9
Aysad Kozanoglu <aysadx@gmail.com>
2018-12-17 02:13:14 +0100

    Create webvirtmgr-install-part1.sh

---
+++ webvirtmgr/webvirtmgr-install-part1.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Author: Aysad Kozanoglu
+
+# set locale en_EN
+wget -O - https://git.io/fpbwk | sh
+
+bash -x /etc/bash.bashrc
+
+apt update
+
+# install KVM HOST libvirt  server headless 
+wget -O - https://git.io/fpAfx | sudo sh
+
+# needed packages for webvirtmgr
+apt-get install git python-pip python-libvirt python-libxml2 novnc supervisor nginx -y --yes
+
+# ask to reboot server 
+wget -qO reboot.sh https://git.io/fpNpo && chmod a+x reboot.sh ; sh reboot.sh
+
+# next step part 2
