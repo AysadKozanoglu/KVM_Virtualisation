d3b1f4ed82f202cddbcad44469122588c4bc2d2a
Aysad Kozanoglu <aysadx@gmail.com>
2018-12-17 13:04:22 +0100

    Update webvirtmgr-install-part2.sh

--- webvirtmgr/webvirtmgr-install-part2.sh
+++ webvirtmgr/webvirtmgr-install-part2.sh
@@ -4,6 +4,8 @@
 # Quick Launch script
 #  wget -O - https://git.io/fpAJA | sh
 
+PYTHONBIN=$(which python)
+
 cd /var/www
 
 git clone git://github.com/retspen/webvirtmgr.git
@@ -13,12 +15,15 @@ cd /var/www/webvirtmgr
 pip install -r requirements.txt 
 
 # set your username / password for webvirtMgr
+$PYTHONBIN /var/www/webvirtmgr/manage.py createsuperuser
+
+# syncdb 
 # yes
-/var/www/webvirtmgr/manage.py syncdb
+$PYTHONBIN /var/www/webvirtmgr/manage.py syncdb
 
 # initial collection
 # yes
-/var/www/webvirtmgr/manage.py collectstatic
+$PYTHONBIN /var/www/webvirtmgr/manage.py collectstatic
 
 chown -R www-data:www-data /var/www/webvirtmgr
 
@@ -27,26 +32,35 @@ rm /etc/nginx/sites-enabled/default
 # get webvirtmgr nginx config port 80
 wget -O /etc/nginx/conf.d/webvirt.conf "https://git.io/fpNhE"; nginx -t && nginx -s reload
 
-service supervisor stop
+## skipping supervisor part
+#
+# service supervisor stop
+## get supervisor config file
+# wget -O /etc/supervisor/conf.d/webvirtmgr.conf "https://git.io/fpNhi"
+# service supervisor start
+# service supervisor status
+# killall python ; killall /usr/bin/python
+##
 
-# get supervisor config file
-wget -O /etc/supervisor/conf.d/webvirtmgr.conf "https://git.io/fpNhi"
+cat /etc/group | grep libvirtd > /dev/null && usermod -a -G libvirtd www-data && id www-data || usermod -a -G libvirt www-data && id www-data
 
-service supervisor start
-
-service supervisor status
-
-killall python ; killall /usr/bin/python
-
-usermod -a -G libvirtd www-data && id www-data
+# ubuntu
+#usermod -a -G libvirtd www-data && id www-data
+# debian
+#usermod -a -G libvirt www-data && id www-data
 
 # run as user www-data 
-sudo  -u www-data bash -c "/usr/bin/python /var/www/webvirtmgr/console/webvirtmgr-console >> /var/www/webvirtmgr/webvirtmgrService.log 2>&1 &" 
+#sudo  -u www-data bash -c "/usr/bin/python /var/www/webvirtmgr/console/webvirtmgr-console >> /var/www/webvirtmgr/webvirtmgrService.log 2>&1 &" 
 
 # run as user www-data 
-sudo  -u www-data bash -c "/usr/bin/python /var/www/webvirtmgr/manage.py runserver 127.0.0.1:8000 >> /var/www/webvirtmgr/webvirtmgrService.log 2>&1 & "  
+#sudo  -u www-data bash -c "/usr/bin/python /var/www/webvirtmgr/manage.py runserver 127.0.0.1:8000 >> /var/www/webvirtmgr/webvirtmgrService.log 2>&1 & "  
 
 # download iso image debian stretch  for  webgui selection (storage > ISO)
 wget -qO /var/www/webvirtmgr/images/debian-9.6.0-amd64-netinst.iso https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-9.6.0-amd64-netinst.iso
 
-# end of installation open in browser your ip or fpdn(if server_name configured in nginx conf)
+echo "=========================================="
+echo "finished. open your ip or fqdn on browser"
+echo "=========================================="
+
+# end of installation 
+# open in browser your ip or fpdn(if server_name configured in nginx conf)
